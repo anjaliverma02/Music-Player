1fc402abeabf2f5d328727836dd9f5fa2ee9093c
Hi, I'm Anjali Verma <vermanjali2704@gmail.com>
2026-06-17 00:27:01 +0530

    Create style.css

---
+++ style.css
@@ -0,0 +1,49 @@
+*{
+    margin:0;
+    padding:0;
+    box-sizing:border-box;
+    font-family:Arial;
+}
+
+body{
+    height:100vh;
+    display:flex;
+    justify-content:center;
+    align-items:center;
+    background:linear-gradient(120deg,#667eea,#764ba2);
+}
+
+.player{
+    width:350px;
+    padding:30px;
+    background:white;
+    text-align:center;
+    border-radius:20px;
+    box-shadow:0 0 20px #333;
+}
+
+#cover{
+    width:200px;
+    height:200px;
+    border-radius:50%;
+    margin:20px;
+}
+
+h2{
+    margin:20px;
+}
+
+button{
+    font-size:25px;
+    padding:10px 20px;
+    margin:5px;
+    border:none;
+    border-radius:50%;
+    background:#667eea;
+    color:white;
+    cursor:pointer;
+}
+
+button:hover{
+    background:#764ba2;
+}
